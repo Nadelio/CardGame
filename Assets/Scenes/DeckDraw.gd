dce90962aa5e63ba4d6a6dcd2f92c58a8d9cb879
Nadelio <carefulsniffle@gmail.com>
2023-10-12 15:10:28 -0500

    Fixed Bug

--- Assets/Scenes/DeckDraw.gd
+++ Assets/Scenes/DeckDraw.gd
@@ -5,7 +5,7 @@ var Decksize = INF
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	scale *= $'../../'.CardSize/size
+	scale *= playspace.CardSize/size
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -15,6 +15,6 @@ func _process(delta):
 func _gui_input(event):
 	if Input.is_action_just_released("leftclick"):
 		if Decksize > 0:
-			Decksize = $'../../'.drawcard()
+			Decksize = playspace.drawcard()
 			if Decksize == 0:
 				disabled = true
